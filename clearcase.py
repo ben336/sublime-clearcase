4d313a0566cbf9d9d002630480484439f7392084
Ben McCormick <ben.mccormick@alumni.duke.edu>
2013-09-10 23:20:09 -0400

    Trying to make checkout linux compatible

--- clearcase.py
+++ clearcase.py
@@ -30,7 +30,7 @@ class ClearcaseExplorerCommand(ClearcaseCommand):
 
 class ClearcaseCheckoutCommand(ClearcaseCommand):
     def run(self):
-        cmd = ['cleartool', 'co -nc -unr', self.window.active_view().file_name()]
+        cmd = ['cleartool', 'co', '-nc', '-unr', self.window.active_view().file_name()]
         super(ClearcaseCheckoutCommand, self).run(cmd)
 
 
